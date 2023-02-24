edd55d8c82ff9b690fb67cf4bbc107c27a831492
dhiraj_thakare_office <88828651+DhirajLucidux@users.noreply.github.com>
2023-02-24 18:38:45 +0530

    updated code

--- chatApp/index.js
+++ chatApp/index.js
@@ -1,9 +1,7 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require("socket.io")(http, {
-  cors: {
-    origins: ["https://angular-chat-app-og69.vercel.app","http://localhost:4200"],
-  },
+  cors: {},
 });
 const port = process.env.PORT || 5000;
 
